b3798685aabb62ee15c4e26e1c7078728356745e
Azuros <55440418+kristoftorok@users.noreply.github.com>
2024-11-29 14:27:27 +0000

    Update index.js

--- js/index.js
+++ js/index.js
@@ -47,7 +47,8 @@ replaceButton.addEventListener("click", async () => {
         "https://fxtelekom.org/ips/websupportsk.txt",
         "https://fxtelekom.org/ips/gcore.txt",
         "https://fxtelekom.org/ips/fastly.txt",
-        "https://fxtelekom.org/ips/cloudflare.txt"
+        "https://fxtelekom.org/ips/cloudflare.txt",
+        "https://fxtelekom.org/ips/github-ucontent.txt"
     ];
     const dnsIPsUrl = "https://fxtelekom.org/ips/dns.txt";
 
