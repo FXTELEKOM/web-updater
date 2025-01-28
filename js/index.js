af950aa13ff65a76f5eee86721654bb71918d35b
kristoftorok <tkristof@protonmail.ch>
2025-01-28 15:45:43 +0100

    Github removed

--- js/index.js
+++ js/index.js
@@ -47,7 +47,6 @@ replaceButton.addEventListener("click", async () => {
         "https://fxtelekom.org/ips/gcore.txt",
         "https://fxtelekom.org/ips/fastly.txt",
         "https://fxtelekom.org/ips/cloudflare.txt",
-        "https://fxtelekom.org/ips/github-ucontent.txt",
         "https://fxtelekom.org/ips/gfn.txt"
     ];
     const dnsIPsUrl = "https://fxtelekom.org/ips/dns.txt";
